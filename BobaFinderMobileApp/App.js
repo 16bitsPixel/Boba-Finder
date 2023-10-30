ebfe79f3801686d2263354f78fdafc6d3bae78c0
Michael Lee <mlee209@ucsc.edu>
2023-10-30 02:02:22 -0700

    added component imports to app.js

--- BobaFinderMobileApp/App.js
+++ BobaFinderMobileApp/App.js
@@ -4,6 +4,11 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import HomeScreen from './components/Home/HomeScreen';
+import CustomScreen from './components/Custom/CustomScreen';
+import TeaOptionsScreen from './components/Custom/TeaOptionsScreen';
+import ToppingOptionsScreen from './components/Custom/ToppingOptionsScreen';
+
 const Stack = createNativeStackNavigator();
 
 // function to run the app, works as a navigator for all our pages
