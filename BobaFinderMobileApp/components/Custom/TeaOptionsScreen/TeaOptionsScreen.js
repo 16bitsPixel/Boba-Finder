39552e4673691997cbc0e2a1a6c4d58fa6e05993
16bitsPixel <brandonllanes16@gmail.com>
2023-11-02 18:01:51 -0700

    fix spacing of tea options

--- BobaFinderMobileApp/components/Custom/TeaOptionsScreen/TeaOptionsScreen.js
+++ BobaFinderMobileApp/components/Custom/TeaOptionsScreen/TeaOptionsScreen.js
@@ -26,7 +26,7 @@ export default function TeaOptionsScreen() {
                         milkTeaList.map(tea => {
                             return (
                                 <TouchableOpacity style={styles.customizationButton} key={tea.id}>
-                                    <Text style = {{fontSize: 20, marginLeft: "5%"}}>
+                                    <Text style = {{fontSize: 20, marginLeft: "5%", flex: 1}}>
                                         {tea.name}
                                     </Text>
                                     <Image source={images.basecup}
@@ -46,7 +46,7 @@ export default function TeaOptionsScreen() {
                     fruitTeaList.map(tea => {
                         return (
                             <TouchableOpacity style={styles.customizationButton} key={tea.id}>
-                                <Text style = {{fontSize: 20, marginLeft: "5%"}}>
+                                <Text style = {{fontSize: 20, marginLeft: "5%", flex: 1}}>
                                     {tea.name}
                                 </Text>
                                 <Image source={images.basecup}
@@ -66,7 +66,7 @@ export default function TeaOptionsScreen() {
                     smoothiesList.map(tea => {
                         return (
                             <TouchableOpacity style={styles.customizationButton} key={tea.id}>
-                                <Text style = {{fontSize: 20, marginLeft: "5%"}}>
+                                <Text style = {{fontSize: 20, marginLeft: "5%", flex: 1}}>
                                     {tea.name}
                                 </Text>
                                 <Image source={images.basecup}
@@ -97,7 +97,7 @@ export default function TeaOptionsScreen() {
     return (
         <SafeAreaView style={styles.container}>
           <ScrollView
-            contentInsetAdjustmentBehavior="automatic">
+            contentInsetAdjustmentBehavior="automatic" style = {{width: "100%"}} >
               <Accordion
                 align="bottom"
                 sections={sections}
