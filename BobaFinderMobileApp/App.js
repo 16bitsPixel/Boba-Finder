5876e0e71e444e136a6b92bb483398f739466286
cwong5768 <cwong5768@gmail.com>
2023-11-08 23:54:40 -0800

    Added splash + fade animation, Changed HomeScreen, Added Favorites to nav bar
    Changed splash.png + added fade animation in App.js
    Added Favorites Screen to nav bar in App.js
    Changed HomeScreen to the version on SplashScreen branch

--- BobaFinderMobileApp/App.js
+++ BobaFinderMobileApp/App.js
@@ -5,23 +5,125 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CustomStack from './components/Custom/CustomScreenStack'
 import HomeScreen from './components/Home/HomeScreen';
+import FavScreen from './components/Favorite/FavoriteScreen'
+
+import {
+  View,
+  Text,
+  Button,
+  Pressable,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Animated,
+  Platform
+} from 'react-native'
+
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+/* import * as Updates from "expo-updates"; */
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 const Drawer = createDrawerNavigator();
+
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
 // Stack for Custom Screen to be nested inside TAB navigation in App.js
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: "#C5E7E2"
-          }
-        }}>
-        <Drawer.Screen name="Home" component={ HomeScreen }/>
-        <Drawer.Screen name="Drink Maker" component={ CustomStack }/>
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <AnimatedAppLoader image={{ uri: "https://raw.githubusercontent.com/16bitsPixel/Boba-Finder/SplashScreen/BobaFinderMobileApp/assets/splash.png" }}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: "#C5E7E2"
+            }
+          }}>
+          <Drawer.Screen name="Home" component={ HomeScreen }/>
+          <Drawer.Screen name="Drink Maker" component={ CustomStack }/>
+          <Drawer.Screen name="Favorites" component={ FavScreen }/>
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </AnimatedAppLoader>
+  );
+}
+
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.fromURI(image.uri).downloadAsync();
+      setSplashReady(true);
+    }
+
+    prepare();
+  }, [image]);
+
+  if (!isSplashReady) {
+    return null;
+  }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 2500, /* Change duration here! */
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.expoConfig.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
   );
 }
--- BobaFinderMobileApp/components/Home/HomeScreen.js
+++ BobaFinderMobileApp/components/Home/HomeScreen.js
@@ -1,19 +1,91 @@
-import React from 'react';
-import { 
+import React from 'react'
+import {
   View,
   Text,
   Button,
-} from 'react-native';
+  Pressable,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Animated,
+  Platform
+} from 'react-native'
+
+import { useFonts } from 'expo-font';
+
+const image = { uri: "https://raw.githubusercontent.com/16bitsPixel/Boba-Finder/main/imgs/splashbg.png" };
 
 
 export default function HomeScreen({ navigation }) {
+  const [loaded] = useFonts({
+    'Assistant': require('../../assets/fonts/Assistant-Light.ttf'),
+    'ComingSoon': require('../../assets/fonts/ComingSoon-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-        <Button
-          title="Drink Maker"
-          onPress={() => navigation.navigate("Drink Maker")}
-        />
-    </View>
+    <View style={styles.container}>
+        <View style={{
+          flex: 1,
+          width: "100%",
+        }}>
+          <ImageBackground source = {image} resizeMode="cover" style={{ flex: 1 }}>
+            <View style={{
+              flexDirection: "column",
+              flex: 1,
+            }}>
+              <Text style={{
+                  fontFamily: 'ComingSoon',
+                  color: 'black',
+                  fontSize: 65,
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                  marginTop: '45%', /* Used to be 48 when header was shown */
+              }}>BobaSpot</Text>
+
+              {/* button for Search, this should move to the Customs screen */}
+              <Pressable style={{
+              backgroundColor: "#C5E7E2",
+              width: "60%",
+              height: "10%",
+              borderRadius: "6px",
+              marginTop: '4%',
+              marginLeft: '20%',
+              justifyContent: "center",
+              alignItems: "center"
+              }} onPress={() => navigation.navigate("Drink Maker")}>
+              <Text style={{ fontFamily: 'ComingSoon', fontSize: 30 }}>Search</Text>
+              </Pressable>
+
+              {/* button for Favorites, this should move to the Favorites screen */}
+              <Pressable style={{
+              backgroundColor: "#C5E7E2",
+              width: "60%",
+              height: "10%",
+              borderRadius: "6px",
+              marginTop: '4%',
+              marginLeft: '20%',
+              justifyContent: "center",
+              alignItems: "center"
+              }} onPress={() => navigation.navigate("Favorites")}>
+              <Text style={{ fontFamily: 'ComingSoon', fontSize: 30 }}>Favorites</Text>
+              </Pressable>
+
+            </View>
+          </ImageBackground>
+        </View>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
