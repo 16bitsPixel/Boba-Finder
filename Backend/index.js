b2fee895166f9ccdb1ef940f1ee2c9d6c134925e
itsPixel <brandonllanes16@gmail.com>
2023-11-09 14:12:34 -0800

    connect server to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+app.get('/', (req, resp) => {
+    console.log(req);
+    return resp.status(234).send("Hello World! Server is up");
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
