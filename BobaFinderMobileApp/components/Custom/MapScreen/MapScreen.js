a3da4786bb49942ca251ee90a4b4d907eeea89e5
cwong5768 <cwong5768@gmail.com>
2023-11-13 23:28:13 -0800

    added map in mapscreen

--- BobaFinderMobileApp/components/Custom/MapScreen/MapScreen.js
+++ BobaFinderMobileApp/components/Custom/MapScreen/MapScreen.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import {
     View,
     Text,
     SafeAreaView,
+    StyleSheet,
 } from 'react-native';
 
 import styles from './MapScreenStyles';
@@ -10,11 +12,14 @@ import styles from './MapScreenStyles';
 export default function MapScreen() {
     return (
         <SafeAreaView style={styles.container}>
-          <View>
-            <Text>
-                MapScreen
-            </Text>
-          </View>
+          <MapView                        /* Map currently just shows user location */
+            style={styles.map}            /* Need to grab user location to show direction */
+            provider={ PROVIDER_GOOGLE }
+            showsUserLocation={true} 
+          />
+          <Text>
+              MapScreen
+          </Text>
         </SafeAreaView>
     );
 }
