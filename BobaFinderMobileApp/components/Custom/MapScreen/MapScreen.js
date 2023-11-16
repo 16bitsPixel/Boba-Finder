5ae0530e8f5e929c6e4824e154deb57d08723cf3
cwong5768 <cwong5768@gmail.com>
2023-11-16 14:38:20 -0800

    added markers to map + changed initial location

--- BobaFinderMobileApp/components/Custom/MapScreen/MapScreen.js
+++ BobaFinderMobileApp/components/Custom/MapScreen/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, Heatmap, PROVIDER_GOOGLE } from 'react-native-maps';
 import {
     View,
     Text,
@@ -10,13 +10,40 @@ import {
 import styles from './MapScreenStyles';
 
 export default function MapScreen() {
+  const [mapLat, setMapLat] = useState(37.0016); /* Map initial location: Santa Cruz College Nine */
+  const [mapLong, setMapLong] = useState(-122.0573);
+
+  const locationData = [
+    {latitude: 36.9650, longitude: -122.0413}, /* Marker 1:  Tzone */
+    {latitude: 36.9640, longitude: -122.0244}, /* Marker 2: Ume Tea */
+  ];
+
+
     return (
         <SafeAreaView style={styles.container}>
           <MapView                        /* Map currently just shows user location */
             style={styles.map}            /* Need to grab user location to show direction */
             provider={ PROVIDER_GOOGLE }
             showsUserLocation={true} 
-          />
+            initialRegion={{
+              latitude: mapLat,
+              longitude: mapLong,
+              latitudeDelta: 0.0922, /* This configures the user's view of the map */
+              longitudeDelta: 0.0421, /* This configures the user's view of the map */
+            }}
+            >
+            {locationData.map((data, index) => (
+                <Marker
+                  key={index}
+                  coordinate={{
+                    latitude: data.latitude,
+                    longitude: data.longitude,
+                  }}
+                  title={`Marker ${index + 1}`}
+                  description={`Weight: ${data.weight}`}
+                />
+              ))}
+            </MapView>
           <Text>
               MapScreen
           </Text>
