2f65bdd9ae7a3cd788fa57cd2b36f26b82b551da
mlee2002 <MKLEE6198@GMAIL.COM>
2023-11-18 04:08:33 -0800

    stores menu done

--- BobaFinderMobileApp/components/Custom/StoresMenu/StoresMenu.tsx
+++ BobaFinderMobileApp/components/Custom/StoresMenu/StoresMenu.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useRef, useMemo, useEffect, useState } from "react"
 import {
     View,
     Text,
+    Image,
     FlatList,
     TouchableOpacity,
 } from 'react-native';
@@ -9,7 +10,8 @@ import axios from 'axios';
 import BottomSheet, { BottomSheetFlatList, BottomSheetScrollView, BottomSheetView } from "@gorhom/bottom-sheet";
 
 import styles from './StoresMenuStyles';
-import stores from '../../../data/stores.json';
+import StarRating from "./Rating";
+import { images } from "../../../constants";
 
 export default function StoresMenu() {
     // make an array for the shops
@@ -31,6 +33,7 @@ export default function StoresMenu() {
 
     const getMovies = async () => {
         try {
+            // use iPv4 address
             const response = await fetch('http://10.0.0.112:5555/shops');
             const json = await response.json();
             setData(json);
@@ -57,28 +60,64 @@ export default function StoresMenu() {
         console.log("handleSheetChange", index);
     }, []);
 
-    // render card from data.json
+    // render store card from data.json
     const renderItem = useCallback(
         (item) => (
             <TouchableOpacity
                 style={styles.card}
                 key={item.id}
                 // TODO: pressing a store card will route the user to the store's pin on the map
-                onPress={() => {}}
+                onPress={() => { }}
             >
-                <Text style={styles.cardText}>
-                    {item.restaurantName}, {item.address}
-                    {"\n"}
-                    Bases: {item.teaBases.join(', ')}
-                    {"\n"}
-                    Toppings: {item.teaToppings.join(', ')}
-                </Text>
+                {/* Left side: Image */}
+                <Image source={ images.basecup } style={styles.image} resizeMode="contain" />
+
+                {/* Right side: Details */}
+                <View style={styles.detailsContainer}>
+                    {/* Store Name */}
+                    <Text style={styles.storeName}>
+                        {item.restaurantName}
+                    </Text>
+
+                    {/* Rating */}
+                    <View style={styles.ratingContainer}>
+                        {/* Render stars based on store.rating */}
+                        {/* You can use an external library or create your own component */}
+                        {/* Example: */}
+                        {/* <StarRating rating={store.rating} /> */}
+                        <Text>
+                            <StarRating rating="5"/>
+                        </Text>
+                    </View>
+
+                    {/* Description */}
+                    <Text style={styles.description}>
+                        {/* TODO: show matching bases */}
+                        Bases: {item.teaBases.slice(0, 2).join(', ')}
+                        {"\n"}
+                        {/* TODO: show matching toppings */}
+                        Toppings: {item.teaToppings.slice(0, 2).join(', ')}
+                    </Text>
+
+                    {/* Store Hours and Distance */}
+                    <View style={styles.infoContainer}>
+                        {/* TODO: show store operational hours */}
+                        <Text style={styles.hours}>
+                            12:00AM-11:59PM
+                        </Text>
+                        {/* TODO: show euclidean distance from user to store */}
+                        <Text style={styles.distance}>
+                            x miles away
+                        </Text>
+                    </View>
+                </View>
             </TouchableOpacity>
         ),
         []
     );
     return (
         <BottomSheet
+            handleStyle={styles.handle}
             ref={sheetRef}
             index={1}
             snapPoints={snapPoints}
--- BobaFinderMobileApp/components/Custom/StoresMenu/StoresMenuStyles.js
+++ BobaFinderMobileApp/components/Custom/StoresMenu/StoresMenuStyles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
 
 export default styles = StyleSheet.create({
+  handle: {
+    backgroundColor: '#C5E7E2',
+  },
   container: {
     backgroundColor: '#C5E7E2',
     alignItems: 'center',
@@ -19,8 +22,47 @@ export default styles = StyleSheet.create({
     flexDirection: 'row',
     alignContent: 'center',
     justifyContent: 'center',
-},
-cardText: {
-    fontSize: 15,
-}
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  detailsContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  storeName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  hours: {
+    fontSize: 12,
+    color: '#555',
+  },
+  distance: {
+    fontSize: 12,
+    color: '#555',
+  },
 })
