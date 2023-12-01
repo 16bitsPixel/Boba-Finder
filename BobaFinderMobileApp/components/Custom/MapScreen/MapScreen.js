48ef71a842fa84fb8c6591570aeade172881b047
cwong5768 <cwong5768@gmail.com>
2023-11-30 16:46:09 -0800

    added distance formula

--- BobaFinderMobileApp/components/Custom/MapScreen/MapScreen.js
+++ BobaFinderMobileApp/components/Custom/MapScreen/MapScreen.js
@@ -5,8 +5,11 @@ import {
     Text,
     SafeAreaView,
     StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
 
+/*import Geolocation from 'react-native-geolocation-service';*/
+import * as Location from 'expo-location';
 
 import styles from './MapScreenStyles';
 
@@ -14,7 +17,10 @@ import styles from './MapScreenStyles';
 export default function MapScreen() {
   const [mapLat, setMapLat] = useState(37.0016); /* Map initial location: Santa Cruz College Nine */
   const [mapLong, setMapLong] = useState(-122.0573);
-
+  const [location, setLocation] = useState(null);
+  const [userLatitude, setUserLatitude] = useState(null);
+  const [userLongitude, setUserLongitude] = useState(null);
+  const [distance, setDistance] = useState(null)
 
   const markers = [
     {
@@ -29,6 +35,42 @@ export default function MapScreen() {
     },
     // Add more markers as needed
   ];
+
+  useEffect(() => {
+    // Gets permission from user to get user location
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        console.log("Please grant location permissions");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setUserLatitude(location.coords.latitude)
+      setUserLongitude(location.coords.longitude)
+      console.log("Location:");
+      console.log(location);
+    };
+    getPermissions();
+
+    //Calculates distance between Marker at index and User Location
+    const calcDistance = async (index) => {
+      var R = 3958.8; // Radius of the Earth in miles
+      var rlat1 = markers[index].coordinate.latitude * (Math.PI/180); // Convert degrees to radians
+      var rlat2 = userLatitude * (Math.PI/180); // Convert degrees to radians
+      var difflat = rlat2-rlat1; // Radian difference (latitudes)
+      var difflon = (markers[index].coordinate.longitude-userLongitude) * (Math.PI/180); // Radian difference (longitudes)
+
+      var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+      setDistance(d.toFixed(2))
+      console.log(d.toFixed(2));
+      // document.getElementById('msg').innerHTML = "Distance between markers: " + distance.toFixed(2) + " mi.";
+    };
+    var ind = 1; //index = 1, change to do a loop and do it for all markers
+    calcDistance(ind);
+  },  []);
  
   const renderMarkers = () => {
     return markers.map((marker, index) => (
@@ -57,9 +99,9 @@ export default function MapScreen() {
             >
               {renderMarkers()}
             </MapView>
-          <Text>
-              Make another view thing and put the stores here
-          </Text>
+          <Text>Distance to marker: {distance} mi</Text>
+          <Text>Latitude: {userLatitude}</Text>
+          <Text>Longitude: {userLongitude}</Text>
         </SafeAreaView>
     );
 }
