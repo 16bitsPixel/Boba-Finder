0ce844207022e375adf9933ed57058f19b9d6635
itsPixel <brandonllanes16@gmail.com>
2024-01-03 15:50:18 -0800

    add map to storeslist screen and use absolute positioning

--- BobaFinderFrontend/screens/drinkMaker/storesListScreen.js
+++ BobaFinderFrontend/screens/drinkMaker/storesListScreen.js
@@ -1,10 +1,16 @@
 import { StyleSheet, View, Text, SafeAreaView, TextInput, StatusBar, Image, ImageBackground, TouchableOpacity } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useEffect, useState } from "react";
 
 // import images
 const searchBGImage = require("../../assets/searchbg.png");
 const backArrow = require("../../assets/images/arrowLeft.png")
 
 export default function StoresListScreen({ navigation }) {
+	// variables and states
+	const [mapLat, setMapLat] = useState(37.3387); /* Map initial location latitude : San Jose */
+	const [mapLong, setMapLong] = useState(-121.8853); /* Map initial location longitude : San Jose */
+
 	return (
 		<SafeAreaView style = {styles.safeContainer}>
 
@@ -21,13 +27,19 @@ export default function StoresListScreen({ navigation }) {
 			</View>
 
 
-			{/* section for background image container
-					inside: bag with boba tea custom options
-			*/}
-			<ImageBackground source = {searchBGImage} style = {{flex: 10}}>
+			<MapView                        /* Map currently just shows user location */
+				style={styles.map}            /* Need to grab user location to show direction */
+				provider={PROVIDER_GOOGLE}
+				showsUserLocation={true}
+				initialRegion={{
+				latitude: mapLat,
+				longitude: mapLong,
+				latitudeDelta: 0.0922, /* This configures the user's view of the map */
+				longitudeDelta: 0.0421, /* This configures the user's view of the map */
+				}}
+			>
 
-				
-			</ImageBackground>
+			</MapView>
 		</SafeAreaView>
 	);
 }
@@ -44,10 +56,10 @@ const styles = StyleSheet.create({
 
 	// styles for the location input at top of screen
 	locationContainer: {
-		flex: 1, 
 		alignItems: "center",
         flexDirection: "row",
-		paddingBottom: 10
+		height: "10%",
+		paddingBottom: 5
 	},
 
 	locationInput: {
@@ -62,4 +74,12 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		marginLeft: 50
 	},
+
+	// styles for the map
+	map: {
+        height: '90%',
+        width: '100%',
+		top: "17%",
+		position: "absolute",
+    },
 });
